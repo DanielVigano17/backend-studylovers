ac557f133a473e05ff4417ddaac306a06f375a53
Daniel Vigano <danielvigano17@gmail.com>
2025-04-07 00:44:30 -0300

    Feat: adiciona atributo de senha no model user

--- src/main/java/com/studylovers/br/backend/model/User.java
+++ src/main/java/com/studylovers/br/backend/model/User.java
@@ -11,8 +11,17 @@ public class User {
     private Long id;
     private String name;
     private String email;
+    private String password;
 
     //Getter e Setters
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
     public Long getId() {
         return id;
     }
